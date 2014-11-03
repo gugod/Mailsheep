6244dbfd513649552b898154e283b3ecd2b1da38
Kang-min Liu <gugod@gugod.org>
2014-11-03 21:24:58 +0100

    show a 7-day histogram.

--- lib/Mailsheep/Cmd/FolderStats.pm
+++ lib/Mailsheep/Cmd/FolderStats.pm
@@ -13,15 +13,32 @@ sub execute {
 
     my @msgcount;
     for (values %folder) {
-        push @msgcount, ["$_", 0+ $_->messages];
+
     }
     @msgcount = sort { $b->[1] <=> $a->[1] } @msgcount;
+
+
+    my $histogram_interval = 7*86400;
+    my %histograms;
+    my $now = time;
+    for my $box (values %folder) {
+        my %hist;
+        for my $message ($box->messages) {
+            my $bucket = int(($now - $message->timestamp)/$histogram_interval);
+            $hist{$bucket}++;
+        }
+
+        $histograms{"$box"} = join ",", map { $hist{$_} } sort { $a <=> $b } keys %hist;
+        my @x = ("$box", 0+$box->messages, $histograms{"$box"});
+        push @msgcount, \@x;
+        # printf("%-20s\t%10d\t%20s\n", @x);
+    }
     $mgr->closeAllFolders;
 
-    printf("%-20s%10s\n","Folder", "Messages");
+    printf("%-20s %10s %s\n","Folder", "Messages", "Histogram");
     printf(("="x30)."\n");
     for (@msgcount) {
-        printf("%-20s%10d\n", $_->[0], $_->[1]);
+        printf("%-20s %10d %s\n", $_->[0], $_->[1], $_->[2]);
     }
 }
 1;
