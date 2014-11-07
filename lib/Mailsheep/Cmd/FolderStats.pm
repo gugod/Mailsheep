a49de12fb469a3fdfafab16b1c8436328c984431
Kang-min Liu <gugod@gugod.org>
2014-11-07 17:06:52 +0100

    let the histogram be an optional output.

--- lib/Mailsheep/Cmd/FolderStats.pm
+++ lib/Mailsheep/Cmd/FolderStats.pm
@@ -1,6 +1,9 @@
 package Mailsheep::Cmd::FolderStats;
 use v5.12;
 use Moo; with('Mailsheep::Role::Cmd');
+
+has histogram => (is => "ro");
+
 sub execute {
     my $self = shift;
 
@@ -13,32 +16,32 @@ sub execute {
 
     my @msgcount;
     for (values %folder) {
-
+        push @msgcount, [ "$_", 0+$_->messages ];
     }
     @msgcount = sort { $b->[1] <=> $a->[1] } @msgcount;
 
-
-    my $histogram_interval = 7*86400;
     my %histograms;
-    my $now = time;
-    for my $box (values %folder) {
-        my %hist;
-        for my $message ($box->messages) {
-            my $bucket = int(($now - $message->timestamp)/$histogram_interval);
-            $hist{$bucket}++;
-        }
+    if (defined($self->histogram)) {
+        my $histogram_interval = ($self->histogram)*86400;
+        my $now = time;
+        for my $box (values %folder) {
+            my %hist;
+            for my $message ($box->messages) {
+                my $bucket = int(($now - $message->timestamp)/$histogram_interval);
+                $hist{$bucket}++;
+            }
 
-        $histograms{"$box"} = join ",", map { $hist{$_} } sort { $a <=> $b } keys %hist;
-        my @x = ("$box", 0+$box->messages, $histograms{"$box"});
-        push @msgcount, \@x;
-        # printf("%-20s\t%10d\t%20s\n", @x);
+            $histograms{"$box"} = join ",", map { $hist{$_} } sort { $a <=> $b } keys %hist;
+        }
     }
+
     $mgr->closeAllFolders;
 
-    printf("%-20s %10s %s\n","Folder", "Messages", "Histogram");
+    printf("%-20s %10s\n","Folder", "Messages");
     printf(("="x30)."\n");
     for (@msgcount) {
-        printf("%-20s %10d %s\n", $_->[0], $_->[1], $_->[2]);
+        my $box = $_->[0];
+        printf("%-20s %10d %s\n", $_->[0], $_->[1], ($histograms{$box}||""));
     }
 }
 1;
