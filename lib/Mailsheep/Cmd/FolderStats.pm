95110acbf0129e342b77f755194a79d9fd9e336d
Kang-min Liu <gugod@gugod.org>
2014-11-09 19:42:19 +0100

    show label stats

--- lib/Mailsheep/Cmd/FolderStats.pm
+++ lib/Mailsheep/Cmd/FolderStats.pm
@@ -14,24 +14,32 @@ sub execute {
         $folder{$x} = $mgr->open("=${x}",  access => "r") or die "The mail box \"=$x\" cannot be opened.\n";
     }
 
-    my @msgcount;
+    my %stats;
+
     for (values %folder) {
-        push @msgcount, [ "$_", 0+$_->messages ];
+        $stats{"$_"} = {
+            total_messages => (0+$_->messages)
+        };
     }
-    @msgcount = sort { $b->[1] <=> $a->[1] } @msgcount;
 
     my %histograms;
-    if (defined($self->histogram)) {
-        my $histogram_interval = ($self->histogram)*86400;
-        my $now = time;
-        for my $box (values %folder) {
-            my %hist;
-            for my $message ($box->messages) {
+    my $histogram_interval = (defined($self->histogram) ? ($self->histogram)*86400 : undef);
+    my $now = time;
+    for my $box (values %folder) {
+        my %hist;
+        my %label;
+        for my $message ($box->messages) {
+            if (defined($self->histogram)) {
                 my $bucket = int(($now - $message->timestamp)/$histogram_interval);
                 $hist{$bucket}++;
             }
-
-            $histograms{"$box"} = join ",", map { $hist{$_} } sort { $a <=> $b } keys %hist;
+            for my $x ($message->labels) {
+                $label{$x}++ if $message->label($x);
+            }
+        }
+        $stats{"$box"}{label} = \%label;
+        if (defined($self->histogram)) {
+            $stats{"$box"}{histogram} = join ",", map { $hist{$_} } sort { $a <=> $b } keys %hist;
         }
     }
 
@@ -39,9 +47,13 @@ sub execute {
 
     printf("%-20s %10s\n","Folder", "Messages");
     printf(("="x30)."\n");
-    for (@msgcount) {
-        my $box = $_->[0];
-        printf("%-20s %10d %s\n", $_->[0], $_->[1], ($histograms{$box}||""));
+    for my $box (keys %stats) {
+        printf("%-20s %10d %s %s\n",
+               "$box",
+               $stats{$box}{total_messages},
+               join(",", map { "$_: " . $stats{$box}{label}{$_} } sort {$stats{$box}{label}{$b} <=> $stats{$box}{label}{$a}  } keys %{$stats{$box}{label}}),
+               ($stats{$box}{histogram}||"")
+           );
     }
 }
 1;
