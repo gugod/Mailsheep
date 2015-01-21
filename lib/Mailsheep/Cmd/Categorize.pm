7185c97af07de23a60e8b0a2619ea8a79dfca491
Kang-min Liu <gugod@gugod.org>
2015-01-21 22:44:40 +0100

    show the field value for reference.

--- lib/Mailsheep/Cmd/Categorize.pm
+++ lib/Mailsheep/Cmd/Categorize.pm
@@ -48,7 +48,7 @@ sub execute {
                 say(join("\t", $category, "==", $answer->{guess}[0]{field}, $mail_message_subject));
             } else {
                 $mgr->moveMessage($folder{$category}, $message) unless $self->dry_run;
-                say(join("\t", $category, "<=", $answer->{guess}[0]{field}, $mail_message_subject));
+                say(join("\t", $category, "<=", $answer->{guess}[0]{field}, join(";", @{$doc->{$answer->{guess}[0]{field}}}), $mail_message_subject));
             }
         } else {
             say(join("\t","(????)", "<=", "(????)", $mail_message_subject));
