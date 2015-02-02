41fbb348294c30465985fead9cd14b5e367c0348
Kang-min Liu <gugod@gugod.org>
2015-02-02 21:57:25 +0100

    add "Received" header.

--- lib/Mailsheep/MailMessageConvertor.pm
+++ lib/Mailsheep/MailMessageConvertor.pm
@@ -3,26 +3,35 @@ use v5.12;
 use Moo::Role;
 use Mailsheep::Analyzer;
 use List::Gen ':modify';
+use List::MoreUtils 'uniq';
 
-sub convert_mail_message_to_document {
-    my (undef, $message) = @_;
-    return {
-        sender     => [ map { $_->address ||"" } $message->sender ],
-        from       => [ map { $_->address ||"" } $message->from   ],
-        from_name  => [ map { $_->name    ||"" } $message->from   ],
-        'list-id'  => [($message->head->study("List-Id")  // "").""],
+sub convert_mail_message_to_analyzed_document {
+    my ($self, $message) = @_;
+    my $doc = {
+        # sender     => [ map { $_->address ||"" } $message->sender ],
+        # 'list-id'  => [($message->head->study("List-Id")  // "").""],
+        fromish => [
+            uniq grep { $_ } map { $_ ? split(/[ \(\)\[\]]/, $_) : () } (
+                (map { ($_->address ||"",  $_->name||"" ) } $message->from),
+                (map { $_->address ||"" } $message->sender),
+            )
+        ],
+        'list-id' => [(($message->head->study("List-Id")  // "")."")],
         'reply-to' => [($message->head->study("reply-to") // "").""],
-        to         => [ map { $_->address ||"" } $message->to     ],
-
+        # to         => [ map { $_->address ||"" } $message->to     ],
         # 'return-path' => [($message->head->study("return-path") // "").""],
         # 'message-id'  => [($message->head->study("message-id") // "").""],
         subject    => [ ($message->head->study("subject")  // "")."" ],
     };
-}
 
-sub convert_mail_message_to_analyzed_document {
-    my ($self, $message) = @_;
-    my $doc = $self->convert_mail_message_to_document($message);
+    my @received = map {
+        my @tok = split(/(\Afrom|by|with|for|;)/, $_);
+        shift @tok;
+        +{ @tok, _raw => "$_" };
+    } $message->head->get("Received");
+
+    $doc->{"received.from"} = [ uniq grep { $_ } map { $_ ? split(/[ \(\)\[\]]/, $_) : () } map { $_->{from} } @received ];
+
     # s/\A.+(\@[^@]+)\z/$1/ for @{$doc->{'return-path'}};
     # s/\A.+(\@[^@]+)\z/$1/ for @{$doc->{'message-id'}};
     for my $h (keys %$doc) {
@@ -39,12 +48,6 @@ sub convert_mail_message_to_analyzed_document {
 
     my @headers = keys %$doc;
     my $doc2 = {};
-    $doc2->{from} = $doc->{from};
-    $doc2->{from_name} = $doc->{from_name};
-    $doc2->{sender} = $doc->{sender};
-    $doc2->{subject} = $doc->{subject};
-
-    # my $doc2 = {%$doc};
     for my $fields (@{scalar cartesian { [$_[0], $_[1]] } (\@headers, \@headers)}) {
         next if $fields->[0] eq $fields->[1];
         if ( @{$doc->{$fields->[0]}} && @{$doc->{$fields->[1]}} ) {
