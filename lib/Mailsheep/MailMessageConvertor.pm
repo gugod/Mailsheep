79f9ae074901d594fd1c6d8c7d473e6460cf7d2b
Kang-min Liu <gugod@gugod.org>
2015-03-13 09:01:16 +0100

    cleanup

--- lib/Mailsheep/MailMessageConvertor.pm
+++ lib/Mailsheep/MailMessageConvertor.pm
@@ -11,21 +11,16 @@ sub convert_mail_message_to_analyzed_document {
         'reply-to' => [($message->head->study("reply-to") // "").""],
         'list-id'  => [(($message->head->study("List-Id") // "")."")],
 
-        'from-asis' => [ $message->head->get("From") ],
-        # 'from.name' => [ (map { ($_->name||"") } $message->from) ],
-        # fromish => [
-        #     uniq grep { $_ } map { $_ ? split(/[ \(\)\[\]]/, $_) : () } (
-        #         (map { ($_->address ||"",  $_->name||"" ) } $message->from),
-        #         (map { $_->address ||"" } $message->sender),
-        #     )
-        # ],
+        fromish => [
+            uniq (
+                $message->head->get("From"),
+                (map { ($_->name||"") ."_". ($_->address||"") } $message->from),
+                (map { ($_->name||"") ."_". ($_->address||"") } $message->sender),
+            )
+        ],
 
-        'sender' => [ map { ($_->name||"") .";". ($_->address||"") } $message->sender ],
-        'to'     => [ map { ($_->name||"") .";". ($_->address||"") } $message->to ],
-        # 'to.name'    => [ map { $_->name    ||"" } $message->to ],
-        # 'to.address' => [ map { $_->address ||"" } $message->to ],
-
-        subject    => [ ($message->head->study("subject")  // "")."" ],
+        'to'     => [ map { ($_->name||"") ."_". ($_->address||"") } $message->to ],
+        subject  => [ ($message->head->study("subject")  // "")."" ],
     };
 
     for my $subject (@{$doc->{subject}}) {
