941de39d2e80d63e134bdcad21425e82874d0b15
Kang-min Liu <gugod@gugod.org>
2016-04-02 01:26:30 +0800

    features: print "distinct features"
    Distinct features are the one only seen in one folder.

--- lib/Mailsheep/App/Command/features.pm
+++ lib/Mailsheep/App/Command/features.pm
@@ -27,42 +27,95 @@ sub opt_spec {
 
 my $JSON = JSON->new->pretty;
 
-sub execute {
-    my ($self, $opt) = @_;
-    my ($folder_name);
-    unless (defined($folder_name = $opt->{folder})) {
-        die "folder is required";
-    }
+sub build_feature_index {
+    my ($self) = @_;
     my $mgr = $self->mail_box_manager;
-    my $folder = $mgr->open("=${folder_name}", access => "rw", remove_when_empty => 0) or die "$folder_name does not exists\n";
-    my $count_message = $folder->messages;
+
     my %features;
-    for (my $i = 0; $i < $count_message; $i++) {
-        my $message = $folder->message($i);
-        my $doc = $self->convert_mail_message_to_analyzed_document( $message );
-        for my $k (keys %$doc) {
-            for my $v (@{ $doc->{$k} }) {
-                my $fk = "$k\t=\t$v";
-                $features{$fk}++;
+    for (@{$self->config->{folders}}) {
+        my $name = $_->{name};
+        my $folder;
+        if ( $name eq $self->{opt}{folder}) {
+            $folder = $self->{folder}
+        } else {
+            $folder = $mgr->open("=${name}", access => "ro", remove_when_empty => 0) or die "$name does not exists\n";
+        }
+        my $count_message = $folder->messages;
+        for (my $i = 0; $i < $count_message; $i++) {
+            my $message = $folder->message($i);
+            my $doc = $self->convert_mail_message_to_analyzed_document( $message );
+            for my $k (keys %$doc) {
+                for my $v (@{ $doc->{$k} }) {
+                    my $fk = "$k\t=\t$v";
+                    $features{$fk}{total}++;
+                    $features{$fk}{by_folder}{$name}++;
+                }
             }
         }
     }
+    $self->{features} = \%features;
+}
+
+sub print_common_features {
+    my ($self) = @_;
+    my $opt = $self->{opt};
+
+    my $count_message = $self->{folder}->messages;
+    my $features = $self->{features};
+
+    my $folder_name = $self->{folder_name};
 
     my $threshold = ($opt->{threshold} > 1) ? $opt->{threshold} : ($count_message * $opt->{threshold});
-    my @fk = sort { $features{$b} <=> $features{$a} } grep { $features{$_} > 2 } keys %features;
+    my @fk = sort { $features->{$b}{by_folder}{$folder_name} <=> $features->{$a}{by_folder}{$folder_name} } grep { $features->{$_}{total} > 2 && $features->{$_}{by_folder}{$folder_name} } keys %$features;
 
     my $bound= 0;
-    while ($features{$fk[$bound]} >= $threshold) {
+    while ($features->{$fk[$bound]}{by_folder}{$folder_name} >= $threshold) {
         $bound++;
     }
     if ( $bound == 0 ) {
         $bound = min(0.1*@fk, 25);
     }
 
+    print "# Common Features\n";
     for (my $i = 0; $i < $bound; $i++) {
         my $k = $fk[$i];
-        printf("%2.2f\t%d\t%d\t\t%s\n", $features{$k} / $count_message, $features{$k}, $count_message, $k);
+        my $f = $features->{$k}{by_folder}{$folder_name};
+        printf("%2.2f\t%d\t%d\t\t%s\n", $f / $count_message, $f, $count_message, $k);
     }
+    print "\n";
+}
+
+sub print_distinct_features {
+    my ($self) = @_;
+
+    my $features = $self->{features};
+    my $count_message = $self->{folder}->messages;
+    my $folder_name = $self->{folder_name};
+
+    print "# Distinct Features\n";
+
+    for my $fk (keys %$features) {
+        my $f = $features->{$fk}{total};
+        next unless $features->{$fk}{by_folder}{$folder_name} && $features->{$fk}{by_folder}{$folder_name} == $f;
+        printf("%2.2f\t%d\t%d\t\t%s\n", $f / $count_message, $f, $count_message, $fk);
+    }
+    print "\n";
+}
+
+sub execute {
+    my ($self, $opt) = @_;
+    my ($folder_name);
+    unless (defined($folder_name = $opt->{folder})) {
+        die "folder is required";
+    }
+    $self->{folder} = $self->mail_box_manager->open("=${folder_name}", access => "ro", remove_when_empty => 0) or die "$folder_name does not exists\n";
+    
+    $self->{opt} = $opt;
+    $self->{folder_name} = $folder_name;
+
+    $self->build_feature_index();
+    $self->print_common_features();
+    $self->print_distinct_features();
 }
 
 1;
