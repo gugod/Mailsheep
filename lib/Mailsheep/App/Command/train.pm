3c69c58881a1b08ea250157e688a139c828b1767
Kang-min Liu <gugod@gugod.org>
2016-07-17 00:14:55 +0200

    Revert "cleanup."
    This reverts commit 60aa9ac12f2cb3b3edb965b7940ad940fd94aa7c.

--- lib/Mailsheep/App/Command/train.pm
+++ lib/Mailsheep/App/Command/train.pm
@@ -12,6 +12,7 @@ use Moo; with(
 );
 
 use Sereal::Encoder;
+use Sereal::Decoder;
 
 sub opt_spec {
     return (
@@ -19,6 +20,9 @@ sub opt_spec {
         [ "folder=s",  "Only train this folder" ]
     );
 }
+
+use Parallel::ForkManager;
+use Mailsheep::Categorizer;
 
 sub execute {
     my ($self, $opt) = @_;
@@ -29,6 +33,31 @@ sub execute {
     $self->remove_noise_features;
     $self->write_feature_index;
     return;
+
+    my $classifier = Mailsheep::Categorizer->new(store => $index_directory);
+
+    my @folders = $opt->{folder} ? ({ name => $opt->{folder} }) : (@{ $self->config->{folders} });
+    my $forkman = Parallel::ForkManager->new( $opt->{workers} ||1 );
+    for my $folder (@folders) {
+        $forkman->start and next;
+        my $folder_name = $folder->{name};
+        say $folder_name;
+        my $folder = $self->mail_box_manager->open("=${folder_name}", access => "r");
+        my $count_message = $folder->messages;
+
+        my @documents;
+        for my $i (0..$count_message-1) {
+            my $message = $folder->message($i);
+            next unless $message->label("seen");
+            my $doc = $self->convert_mail_message_to_analyzed_document( $message );
+            push @documents, $doc;
+        }
+
+        $classifier->train($folder_name, \@documents);
+        $folder->close;
+        $forkman->finish;
+    }
+    $forkman->wait_all_children;
 }
 
 sub build_feature_index {
