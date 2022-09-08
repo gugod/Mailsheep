378890a3f47f6df80e32702e43073f382ac9b84a
Kang-min Liu <gugod@gugod.org>
2022-09-08 23:10:46 +0900

    re-formatted.

--- lib/Mailsheep/MailMessageConvertor.pm
+++ lib/Mailsheep/MailMessageConvertor.pm
@@ -3,18 +3,31 @@ use v5.36;
 use Moo::Role;
 use List::Gen ':modify';
 use List::MoreUtils 'uniq';
+use JSON;
 
 use Importer 'Mailsheep::Analyzer' => 'reduced_mail_subject';
 
+sub true()  { $JSON::true }
+sub false() { $JSON::false }
+
 sub convert_mail_message_to_simple_document ( $self, $message ) {
+    my $head = $message->head;
+    my $body = $message->body;
+
     my $doc = {
-        'reply-to' => [ ( $message->head->study("reply-to") // "" ) . "" ],
-        'list-id'  => [ ( ( $message->head->study("List-Id") // "" ) . "" ) ],
-        'from'     => [ map { $_->format } $message->from ],
-        'sender'   => [ map { $_->format } $message->sender ],
-        'to'       => [ map { $_->format } $message->to ],
-        'subject'  => [ ( $message->head->study("subject") // "" ) . "" ],
-        'body'     => [ "" . $message->body->decoded ],
+        'reply-to' => ["" . ( $head->study("reply-to") // "" )],
+        'list-id'  => ["" . ( $head->study("List-Id")  // "" )],
+        'from'     => [map { $_->format } $message->from],
+        'sender'   => [map { $_->format } $message->sender],
+        'to'       => [map { $_->format } $message->to],
+        'subject'  => ["" . ( $head->study("subject") // "" )],
+
+        # 'body'     => [ "" . $message->body->decoded ],
+        ':ABOUT' => [
+            nrLines         => $body->nrLines,
+            bodyIsMultipart => ( $body->isMultipart ? true : false ),
+            bodyMimeType    => ( "" . $body->mimeType ),
+        ]
     };
 
     for my $h ( keys %$doc ) {
@@ -29,32 +42,23 @@ sub convert_mail_message_to_simple_document ( $self, $message ) {
 
 sub convert_mail_message_to_analyzed_document ( $self, $message ) {
     my $doc = {
-        'reply-to' => [ ( $message->head->study("reply-to") // "" ) . "" ],
-        'list-id'  => [ ( ( $message->head->study("List-Id") // "" ) . "" ) ],
+        'reply-to' => [( $message->head->study("reply-to") // "" ) . ""],
+        'list-id'  => [( ( $message->head->study("List-Id") // "" ) . "" )],
 
         fromish => [
             uniq(
                 $message->head->get("From"),
-                (
-                    map { ( $_->name || "" ) . "_" . ( $_->address || "" ) }
-                      $message->from
-                ),
-                (
-                    map { ( $_->name || "" ) . "_" . ( $_->address || "" ) }
-                      $message->sender
-                ),
+                ( map { ( $_->name || "" ) . "_" . ( $_->address || "" ) } $message->from ),
+                ( map { ( $_->name || "" ) . "_" . ( $_->address || "" ) } $message->sender ),
             )
         ],
 
-        'to' => [
-            map { ( $_->name || "" ) . "_" . ( $_->address || "" ) }
-              $message->to
-        ],
-        '!date' => [ ( !$message->head->get("Date") ) ],
+        'to'    => [map { ( $_->name || "" ) . "_" . ( $_->address || "" ) } $message->to],
+        '!date' => [( !$message->head->get("Date") )],
     };
 
     $doc->{subject} =
-      [ reduced_mail_subject( $message->head->study("subject") ) ];
+        [reduced_mail_subject( $message->head->study("subject") )];
 
     for my $h ( keys %$doc ) {
         for ( @{ $doc->{$h} } ) {
@@ -68,12 +72,12 @@ sub convert_mail_message_to_analyzed_document ( $self, $message ) {
 
     my @headers = keys %$doc;
     my $doc2    = {};
-    ( cartesian { [ sort ( $_[0], $_[1] ) ] } ( \@headers, \@headers ) )->each(
+    ( cartesian { [sort ( $_[0], $_[1] )] } ( \@headers, \@headers ) )->each(
         sub {
             my $fields = $_;
             return if $fields->[0] eq $fields->[1];
             return
-              unless ( @{ $doc->{ $fields->[0] } }
+                unless ( @{ $doc->{ $fields->[0] } }
                 && @{ $doc->{ $fields->[1] } } );
             my $ha = $fields->[0] . "," . $fields->[1];
             return if $doc2->{$ha};
